229a8977f2bb7c8a05f35f2c3aee917ef933452c
ankuri-shiva <ankurishiva@gmail.com>
2024-09-24 17:29:39 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
+
+
+// app.use("/user", (req, res) => {
+//     res.send("Order matters");
+// });
+
+
+app.delete( "/test", (req, res) => {
+    res.send("Hello from the server is deleted");
+});
+// this will only handle GET call to /hello
+app.get("/hello", (req, res) => {
+    res.send({fistname:"shiva", lastname : "ankuri"});
+});
+
+ app.post("/user", (req, res) => {
+    res.send("data successfully saved in database");
+ })
+
+ // This will match all the HTTP method API calls to /test
+ app.use("/", (req, res) => {
     res.send("Home..");
 })
-
-app.use( "/test", (req, res) => {
-    res.send("Hello from the server");
-});
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello...");
-});
-
 
 app.listen(7777, () => {
     console.log("server is running on port no :7777")
